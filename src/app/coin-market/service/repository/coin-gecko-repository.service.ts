f14965fea9782ee6eae7ede812049005bdd4c786
Bastien Decorte <seriousbooster@gmail.com>
2020-10-12 13:54:18 +0200

    Fix missing file

--- src/app/coin-market/service/repository/coin-gecko-repository.service.ts
+++ src/app/coin-market/service/repository/coin-gecko-repository.service.ts
@@ -2,8 +2,8 @@ import {Injectable} from '@angular/core';
 import {HttpClient, HttpResponse} from '@angular/common/http';
 import {CoinDto} from '../../dto/coin-dto';
 import {environment} from '../../../../environments/environment';
-import {Observable, of, throwError} from 'rxjs';
-import {catchError, retry} from 'rxjs/operators';
+import {Observable} from 'rxjs';
+import {retry} from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
@@ -11,7 +11,8 @@ import {catchError, retry} from 'rxjs/operators';
 export class CoinGeckoRepositoryService {
   public COIN_LIST_SUFFIX = 'coins/list';
 
-  constructor(public httpClient: HttpClient) {}
+  constructor(public httpClient: HttpClient) {
+  }
 
   public fetchCoinList(): Observable<HttpResponse<CoinDto[]>> {
     return this.httpClient.get<CoinDto[]>(environment.coingecko_api + this.COIN_LIST_SUFFIX, {observe: 'response'}).pipe(retry(3));
